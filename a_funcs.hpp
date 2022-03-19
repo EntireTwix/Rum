262698ade3e824d6fdb90301a46f02b3f6eb60a6
William Katz <qualitycontentquestionmark@gmail.com>
2022-03-18 21:03:23 -0700

    :fire: removed `A`

--- a_funcs.hpp
+++ a_funcs.hpp
@@ -1,17 +1,15 @@
 #pragma once
 #include <cmath>
 
-constexpr float A = 0.0001;
-
 namespace rum
-{
+{    
     //relu
     constexpr float relu(float x) { return (x > 0) * x; }
     constexpr float relu_prime(float x) { return x > 0; }
 
     //leaky relu
-    constexpr float relu_leaky(float x) { if (x < 0) { return x * A; } else { return x; } }
-    constexpr float relu_leaky_prime(float x) { if (x < 0) { return A; } else { return 1; } }
+    constexpr float relu_leaky(float x) { if (x < 0) { return x * 0.0001; } else { return x; } }
+    constexpr float relu_leaky_prime(float x) { if (x < 0) { return 0.0001; } else { return 1; } }
 
     //tanh
     constexpr float tanh_prime(float x) { return 1 - std::pow(tanh(x), 2); }
