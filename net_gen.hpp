aa4f1ba3a00a7e07b1ab87aad6939268b72bad32
EntireTwix <willkatz.dev@gmail.com>
2022-03-23 13:42:55 -0700

    :sparkles: finished `RUM_FF`

--- net_gen.hpp
+++ net_gen.hpp
@@ -1,16 +1,20 @@
 #pragma once
 #include "third_party/Matrix/std/ml_ops.hpp"
 
-#define LAYER_N(name, number) name##_layer_##number
+#define LAYER_N(net_name, name) net_name##_layer_##name
 
 #define RUM_FF(name, input_size, hidden_size, output_size)\
-IMat<input_size> LAYER_N(name, 0);                        \
-WMat<input_size, hidden_size> LAYER_N(name, 1);           \
-BMat<hidden_size> LAYER_N(name, 2);                       \
-WMat<hidden_size, output_size> LAYER_N(name, 3);          \
-BMat<output_size> LAYER_N(name, 4);                       \
-OMat<output_size> LAYER_N(name, 5);                       \
-OMat<hidden_size> LAYER_N(name, 6);                       \
-OMat<hidden_size> LAYER_N(name, 7);                       \
-OMat<output_size> LAYER_N(name, 8);                       \
-OMat<output_size> LAYER_N(name, 9);                       
+IMat<input_size> LAYER_N(name, inp);                      \
+WMat<input_size, hidden_size> LAYER_N(name, w1);          \
+BMat<hidden_size> LAYER_N(name, b1);                      \
+WMat<hidden_size, output_size> LAYER_N(name, w2);         \
+BMat<output_size> LAYER_N(name, b2);                      \
+OMat<output_size> LAYER_N(name, ans);                     \
+OMat<hidden_size> LAYER_N(name, w1o);                     \
+OMat<hidden_size> LAYER_N(name, h1o);                     \
+OMat<output_size> LAYER_N(name, w2o);                     \
+OMat<output_size> LAYER_N(name, out);                     \
+BMat<output_size> LAYER_N(name, out_error);               \
+WMat<hidden_size, output_size> LAYER_N(name, w2_error);   \
+BMat<hidden_size> LAYER_N(name, h1_error);                \
+WMat<input_size, hidden_size> LAYER_N(name, w1_error);    \
