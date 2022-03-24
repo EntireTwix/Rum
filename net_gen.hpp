3404f77282bc2134fa3694bbe53395eca360f0fd
EntireTwix <willkatz.dev@gmail.com>
2022-03-23 19:04:13 -0700

    :sparkles: forward prop

--- net_gen.hpp
+++ net_gen.hpp
@@ -18,3 +18,9 @@ BMat<output_size> LAYER_N(name, out_error);               \
 WMat<hidden_size, output_size> LAYER_N(name, w2_error);   \
 BMat<hidden_size> LAYER_N(name, h1_error);                \
 WMat<input_size, hidden_size> LAYER_N(name, w1_error);    
+
+#define RUM_FF_F(name, h1_a, h2_a)                                                            \
+LAYER_N(name, w1o) = WeightForward(LAYER_N(name, inp), LAYER_N(name, w1), LAYER_N(name, b1)); \
+LAYER_N(name, h1o) = HiddenForward(LAYER_N(name, w1o), h1_a);                                 \
+LAYER_N(name, w2o) = WeightForward(LAYER_N(name, h1o), LAYER_N(name, w2), LAYER_N(name, b2)); \
+LAYER_N(name, out) = HiddenForward(LAYER_N(name, w2o), h2_a);
