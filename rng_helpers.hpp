b68139bb26c6e63f89b075b55789425d13c60aad
EntireTwix <willkatz.dev@gmail.com>
2022-03-24 13:05:10 -0700

    :sparkles: Xavier, Xavier Norm, and He Weight init

--- rng_helpers.hpp
+++ rng_helpers.hpp
@@ -27,5 +27,15 @@ namespace rum
     public:
         BasicGen(float low = 0.0f, float high = 1.0f) noexcept : _low(low), _high(high) { assert(this->_high > this->_low); }
         float operator()() const { return (GenerateFloat() * (this->_high - this->_low)) + this->_low; }
-    };
+    }; 
+
+    //for Sigmoid & Tanh
+    template <size_t N>
+    struct XavierWeight : BasicGen { XavierWeight() : BasicGen(-(1.0f/std::sqrt(N)), (1.0f/std::sqrt(N))) {}; };
+    template <size_t N, size_t M>
+    struct NormXavierWeight : BasicGen { NormXavierWeight() : BasicGen(-(std::sqrt(6.0f)/std::sqrt(N * M)), std::sqrt(6)/std::sqrt(N * M)) {}; };
+    
+    //for Relu
+    template <size_t N>
+    struct He : BasicGen { He() : BasicGen(0, std::sqrt(2.0f/N)) {} };
 };
