0d66d30c54d7b20be171b25a7f4f044d1d0172ef
EntireTwix <willkatz.dev@gmail.com>
2022-03-28 20:02:48 -0700

    made small constant, bigger

--- a_funcs.hpp
+++ a_funcs.hpp
@@ -9,15 +9,15 @@ namespace rum
     constexpr float relu_prime(float x) { return x > 0; }
 
     //leaky relu
-    constexpr float relu_leaky(float x) { if (x < 0) { return x * 0.0001; } else { return x; } }
-    constexpr float relu_leaky_prime(float x) { if (x < 0) { return 0.0001; } else { return 1; } }
+    constexpr float relu_leaky(float x) { if (x < 0) { return x * 0.01; } else { return x; } }
+    constexpr float relu_leaky_prime(float x) { if (x < 0) { return 0.01; } else { return 1; } }
 
     //tanh
     constexpr float tanh_prime(float x) { return 1 - pow2<double>(tanh(x)); }
 
     //sigmoid
     constexpr float sigmoid(float x) { return 1 / (1 + exp(-x)); }
-    constexpr float sigmoid_prime(float x) { return exp(-x) / pow2<double>(1 + exp(-x)); } // TODO: benchmark alternatives
+    constexpr float sigmoid_prime(float x) { return sigmoid(x) * (1 - sigmoid(x)); } // TODO: benchmark alternatives
 
     //swish
     constexpr float swish(float x) { return x * sigmoid(x); }
