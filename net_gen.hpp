9a838f493e85426599111706959a7d296e4901cc
William Katz <qualitycontentquestionmark@gmail.com>
2022-04-05 23:58:02 -0700

    :art:

--- net_gen.hpp
+++ net_gen.hpp
@@ -21,7 +21,7 @@ BMat<hidden_size> LAYER_N(name, h1_error);                \
 WMat<input_size, hidden_size> LAYER_N(name, w1_error);    
 
 #define RUM_FF_F(name, input, h1_a, out_a)                                                     \
-LAYER_N(name, w1o) = WeightForward(input, LAYER_N(name, w1), LAYER_N(name, b1));\
+LAYER_N(name, w1o) = WeightForward(input, LAYER_N(name, w1), LAYER_N(name, b1));               \
 LAYER_N(name, h1o) = HiddenForward(LAYER_N(name, w1o), h1_a);                                  \
 LAYER_N(name, w2o) = WeightForward(LAYER_N(name, h1o), LAYER_N(name, w2), LAYER_N(name, b2));  \
 LAYER_N(name, out) = HiddenForward(LAYER_N(name, w2o), out_a);                               
@@ -38,7 +38,7 @@ Learn(LAYER_N(name, w2), LAYER_N(name, w2_error), learning_rate); \
 Learn(LAYER_N(name, b1), LAYER_N(name, h1_error), learning_rate); \
 Learn(LAYER_N(name, w1), LAYER_N(name, w1_error), learning_rate);      
 
-#define RUM_AE(name, input_size, hidden_size)  \
+#define RUM_AE(name, input_size, hidden_size)         \
 WMat<input_size, hidden_size> LAYER_N(name, w1);      \
 BMat<hidden_size> LAYER_N(name, b1);                  \
 WMat<hidden_size, input_size> LAYER_N(name, w2);      \
@@ -55,7 +55,7 @@ WMat<input_size, hidden_size> LAYER_N(name, w1_error);
 #define RUM_AE_F(name, input, h1_a, out_a) RUM_FF_F(name, input, h1_a, out_a)                              
 
 #define RUM_AE_B(name, input, h1_ap, out_ap)                                                                     \
-LAYER_N(name, out_error) = OutputBackward(LAYER_N(name, out), input, LAYER_N(name, w2o), out_ap);   \
+LAYER_N(name, out_error) = OutputBackward(LAYER_N(name, out), input, LAYER_N(name, w2o), out_ap);                \
 LAYER_N(name, w2_error) = WeightBackward(LAYER_N(name, out_error), LAYER_N(name, h1o));                          \
 LAYER_N(name, h1_error) = HiddenBackward(LAYER_N(name, out_error), LAYER_N(name, w2), LAYER_N(name, w1o), h1_ap);\
 LAYER_N(name, w1_error) = WeightBackward(LAYER_N(name, h1_error), input);                        
